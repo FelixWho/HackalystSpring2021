0264aca7d6d0420f009e27bb3d3e59c1b413e3ec
Ahan Kaul <ahan.kaul@duke.edu>
2021-03-27 19:24:37 -0400

    updated info page

--- gatsby-starter-forty/src/pages/information.js
+++ gatsby-starter-forty/src/pages/information.js
@@ -4,8 +4,8 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import BannerLanding from '../components/BannerLanding'
 
-import pic08 from '../assets/images/pic08.jpg'
-import pic09 from '../assets/images/pic09.jpg'
+import pic08 from '../assets/images/imposter3.jpg'
+import pic09 from '../assets/images/felixandfriends.jpg'
 import pic10 from '../assets/images/pic10.jpg'
 
 const Landing = (props) => (
@@ -37,9 +37,6 @@ const Landing = (props) => (
                                 <h3>Imposter Syndrome Workshops</h3>
                             </header>
                             <p>In collaboration with executive coaches in the Research Triangle Park and Amy Arnold from DTech, the Catalyst exec team is putting together a workshop dedicated to demystifying Imposter Syndrome and providing students with resources to develop personally and professionally. We held this event on Thursday, March 18th, and are planning to hold more in the future!</p>
-                            <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
                         </div>
                     </div>
                 </section>
@@ -50,12 +47,9 @@ const Landing = (props) => (
                     <div className="content">
                         <div className="inner">
                             <header className="major">
-                                <h3>Rhoncus magna</h3>
+                                <h3>Hackalyst!</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
+                            <p>As a part of our new member orientation (NME) program, baby catalyst members are thrown headfirst like lambs to the slaughter into a high-stakes, high-pressure hackathon. Members are given 24 hours to produce a project in one of 4 categories: flushed-out wireframe, app, website, or data analysis, after which they are to present a PowerPoint on their product. Pictured on the right is the winning team from the 2021 PC, Felix and Friends: Allison Shi, Chan Park, Ahan Kaul, and Felix Hu. </p>
                         </div>
                     </div>
                 </section>
