31900dfcb77f5328df59197ecde2bdec55dc5795
Chan Park <chanpark@Chans-MBP.wired.duke.local>
2021-03-27 18:26:23 -0400

    worked on members page and changed layout banner image

---
+++ gatsby-starter-forty/src/components/BannerMembers.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BannerMembers = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Members</h1>
+            </header>
+            <div className="content">
+                <p>these are the members lol</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerMembers
