4c18be87d316399b44ce8c0515b4f36b12161b2a
felixwho <felixhu@wustl.edu>
2021-03-27 15:33:04 -0400

    first push

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HackalystSpring2021
+
+### Assets
+
+https://www.figma.com/file/TpaoLH21RHDwpUJbm8duI8/Catalyst-Website-v3?node-id=0%3A1
+
+https://github.com/Catalyst-at-Duke/website-v3
---
+++ gatsby-starter-forty/src/components/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>CATALYST</h1>
+            </header>
+            <div className="content">
+                <p>Duke University's Premier Tech Society.<br />
+                </p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Join the mailing list</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
